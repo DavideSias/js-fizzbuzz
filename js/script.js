69387d7083b769392ebb1d1278acad16d3b476c2
DavideSias <davidesias77@gmail.com>
2022-10-24 16:21:56 +0200

    Refactor js script

--- js/script.js
+++ js/script.js
@@ -15,15 +15,12 @@ ELSE IF eleBuzz === 0 allora stampare 'Buzz'
 
 
 for (i = 1; i <= 100; i++){
-    
-    let eleFizz = i % 3;
-    let eleBuzz = i % 5;
-    
-    if((eleFizz === 0) && (eleBuzz === 0)){
+  
+    if((i % 3 == 0) && (i % 5 == 0)){
         console.log('FizzBuzz')
-    } else if (eleFizz === 0){
+    } else if (i % 3 == 0){
         console.log('Fizz');
-    } else if (eleBuzz === 0){
+    } else if (i % 5 == 0){
         console.log('Buzz');
     } else {
         console.log(i);
